a741a1da0c0d4276ed17d31859a6f58268185e89
Francesco Canessa <makevoid@gmail.com>
2014-05-22 15:59:24 +0200

    poste ita was easy to do, need to finish tough

--- posteitaliane.rb
+++ posteitaliane.rb
@@ -2,3 +2,20 @@ require 'mechanize'
 
 URL = "http://www.poste.it/online/dovequando/home.do"
 
+data = File.read "./data/example_tracking_numbers.txt"
+data = data.split("\n").map(&:strip)
+
+
+agent = Mechanize.new
+agent.user_agent = "Mac Firefox"
+
+poste_url = "http://www.poste.it/online/dovequando/ricerca.do"
+
+params = {}
+1.upto(10).each do |i|
+  params["mpcode#{i}"] = data[i]
+end
+
+page = agent.post poste_url, params
+
+puts page.body
